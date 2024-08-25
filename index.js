3368859e455346c5bd9a83b1f41814097fc5cd3c
Guilherme B <dogui0202@gmail.com>
2024-08-25 14:50:45 -0300

    carrinho já está aparecendo da maneira que eu gostaria

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
-    
-})
+        $('#Carrinho').click(function() {
+            $('.carrinho').animate({
+                width: 'toggle'
+            }, 1300);
+            $('.carrinho').css({
+                display: 'flex'
+            })    
+        });
+        $('#Fechar').click(function() {
+            $('.carrinho').animate({
+                width: 'toggle'
+            }, 1300);
+            $('.carrinho').css({
+                display: 'flex'
+            }, )
+        });
+
+
+
+});
